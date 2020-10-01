ea333920ed18ed6a9cf69cc62d598df47b204dd8
smooney211993 <sm.athome21@gmail.com>
2020-10-01 19:37:18 +1000

    added listgroup

--- client/src/Components/Screens/ProductScreen.js
+++ client/src/Components/Screens/ProductScreen.js
@@ -5,7 +5,26 @@ import Rating from '../Layouts/Rating';
 import products from '../../products';
 const ProductScreen = ({ match }) => {
   const product = products.find((p) => p._id === match.params.id);
-  return <div>{product.name}</div>;
+  return (
+    <>
+      <div>{product.name}</div>
+      <Link to='/' className='btn btn-light my-3'>
+        Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
