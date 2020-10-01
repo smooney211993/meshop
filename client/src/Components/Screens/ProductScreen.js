85a77ab5e590b86d2f1cdb849fc0ef12fde0517e
smooney211993 <sm.athome21@gmail.com>
2020-10-01 19:47:57 +1000

    finished product page

--- client/src/Components/Screens/ProductScreen.js
+++ client/src/Components/Screens/ProductScreen.js
@@ -20,7 +20,44 @@ const ProductScreen = ({ match }) => {
             <ListGroup.Item>
               <h2>{product.name}</h2>
             </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews}`} />{' '}
+              Reviews
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price</Col>
+                  <Col>
+                    <strong>{product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status</Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                    </strong>
+                  </Col>
+                </Row>
+                <ListGroup.Item>
+                  <Button
+                    className='btn-block'
+                    disabled={product.countInStock === 0}>
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </>
