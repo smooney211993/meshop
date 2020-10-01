a3fc065a20345bf3dab3ff994a0404631a9a6c9c
smooney211993 <sm.athome21@gmail.com>
2020-10-01 11:21:51 +1000

    created routing for some components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Components/Layouts/Header';
 import Footer from './Components/Layouts/Footer';
 import Homescreens from './Components/Screens/Homescreens';
+import ProductScreen from './Components/Screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <Container></Container>
       <main>
         <Container>
-          <Homescreens />
+          <Route exact path='/' component={Homescreens} />
+          <Route exact path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
