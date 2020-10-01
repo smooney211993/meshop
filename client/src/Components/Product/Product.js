d4ff9142b39ba6384cfcd4118efd6f2aad9f0379
smooney211993 <sm.athome21@gmail.com>
2020-10-01 10:49:57 +1000

    added product cards

--- client/src/Components/Product/Product.js
+++ client/src/Components/Product/Product.js
@@ -7,6 +7,20 @@ const Product = ({ product }) => {
       <a href={`/products/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </a>
+
+      <Card.Body>
+        <a href={`/products/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as='div'>
+          <div className='my-3'>
+            {product.rating} from {product.numReviews} Reviews
+          </div>
+        </Card.Text>
+        <Card.Text as='h3'>{product.price}</Card.Text>
+      </Card.Body>
     </Card>
   );
 };
