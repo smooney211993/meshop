382e2445c3b4d4a5d5600c0aff47290b747aa9d2
smooney211993 <sm.athome21@gmail.com>
2020-10-03 12:11:35 +1000

    created env variables

--- client/src/Components/Screens/ProductScreen.js
+++ client/src/Components/Screens/ProductScreen.js
@@ -35,7 +35,7 @@ const ProductScreen = ({ match }) => {
                 <Row>
                   <Col>Price</Col>
                   <Col>
-                    <strong>{product.price}</strong>
+                    <strong>${product.price}</strong>
                   </Col>
                 </Row>
               </ListGroup.Item>
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const products = require('./routes/products');
+const dotenv = require('dotenv');
+dotenv.config();
 app.use(express.json());
 app.use('/api/products', products);
 
 app.listen(PORT, () => {
-  console.log(`server ${PORT} is up and running`);
+  console.log(
+    `server ${PORT} is up and running in ${process.env.NODE_ENV} mode`
+  );
 });
