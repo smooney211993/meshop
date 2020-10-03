578e018bc195055b51b76b7cfd6ed73fd14f2f23
smooney211993 <sm.athome21@gmail.com>
2020-10-03 12:41:27 +1000

    added colors to the terminal

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const products = require('./routes/products');
 const dotenv = require('dotenv');
+const colors = require('colors');
 dotenv.config();
 const connectDB = require('./config/db');
 connectDB();
@@ -12,6 +13,7 @@ app.use('/api/products', products);
 
 app.listen(PORT, () => {
   console.log(
-    `server ${PORT} is up and running in ${process.env.NODE_ENV} mode`
+    `server ${PORT} is up and running in ${process.env.NODE_ENV} mode`.yellow
+      .underline.bold
   );
 });
