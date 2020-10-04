5fc07b459e08ded85ef520f75efc7a498186db98
smooney211993 <sm.athome21@gmail.com>
2020-10-04 13:10:51 +1100

    started cart screen

--- client/src/Components/Screens/CartScreen.js
+++ client/src/Components/Screens/CartScreen.js
@@ -1,4 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../../actions/cartActions';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Message from '../Layouts/Message';
 
 const CartScreen = () => {
   return <div></div>;
--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -2,22 +2,24 @@ import axios from 'axios';
 import { CART_ADD_ITEM } from './types';
 
 export const AddToCart = (id, qty) => async (dispatch, getState) => {
-    try {
-        const {data} = await.get(`/api/products${id}`)
-        dispatch({
-            type: CART_ADD_ITEM,
-            payload : {
-                product: data._id,
-                name: data.name,
-                image: data.image,
-                price: data.price,
-                countInStock : data.countInStock,
-                qty
-            }
-        })
-        localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-    } catch (error) {
-        console.log(error.message)
-    }
-    
+  try {
+    const { data } = await axios.get(`/api/products${id}`);
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        product: data._id,
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        countInStock: data.countInStock,
+        qty,
+      },
+    });
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(getState().cart.cartItems)
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
 };
