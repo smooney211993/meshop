489664d37799a833bd94eba08a376f865189e2ff
smooney211993 <sm.athome21@gmail.com>
2020-10-04 12:34:27 +1100

    changed theme

--- client/src/Components/Screens/ProductScreen.js
+++ client/src/Components/Screens/ProductScreen.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from 'react-bootstrap';
 import Rating from '../Layouts/Rating';
 import Spinner from '../Layouts/Spinner';
 import Message from '../Layouts/Message';
@@ -64,6 +72,27 @@ const ProductScreen = ({ match }) => {
                       </strong>
                     </Col>
                   </Row>
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control
+                            as='select'
+                            value={qty}
+                            onChange={(e) => setQty(e.target.value)}>
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
                   <ListGroup.Item>
                     <Button
                       className='btn-block'
