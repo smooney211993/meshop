7b9ffea9880de735e14a9323f5563befb2c8eca3
smooney211993 <sm.athome21@gmail.com>
2020-10-06 10:40:41 +1100

    added checkout button

--- client/src/Components/Screens/CartScreen.js
+++ client/src/Components/Screens/CartScreen.js
@@ -29,6 +29,10 @@ const CartScreen = ({ match, location, history }) => {
   const removeItemHandler = (id) => {
     dispatch(removeItemFromCart(id));
   };
+
+  const checkOutHandler = () => {
+    history.push(`/login?redirect=shipping`);
+  };
   return (
     <Row>
       <Col md={8}>
@@ -45,7 +49,7 @@ const CartScreen = ({ match, location, history }) => {
                   <Col md={2}>
                     <Image src={item.image} alt={item.name} fluid rounded />
                   </Col>
-                  <Col md={2}>
+                  <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
                   <Col md={2}>{item.price}</Col>
@@ -80,6 +84,31 @@ const CartScreen = ({ match, location, history }) => {
             ))}
           </ListGroup>
         )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variat='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                Items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkOutHandler}>
+                Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Col>
     </Row>
   );
