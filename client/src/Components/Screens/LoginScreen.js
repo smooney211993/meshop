51c5829ebfa6a73a245ba864cc9109b1f9c99810
smooney211993 <sm.athome21@gmail.com>
2020-10-06 17:14:46 +1100

    created the log in screen

---
+++ client/src/Components/Screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button, Col, Row } from 'react-router-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+const LoginScreen = () => {
+  return <div></div>;
+};
+
+export default LoginScreen;
