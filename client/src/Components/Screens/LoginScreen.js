aa68c7271ec7cf9601a45bf51bef6bea367916c6
smooney211993 <sm.athome21@gmail.com>
2020-10-06 17:21:53 +1100

    added input form for login form

--- client/src/Components/Screens/LoginScreen.js
+++ client/src/Components/Screens/LoginScreen.js
@@ -2,9 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button, Col, Row } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-
+import Message from '../Layouts/Message';
+import Spinner from '../Layouts/Spinner';
+import FormContainer from '../Layouts/FormContainer';
+import { login } from '../../actions/userActions';
 const LoginScreen = () => {
-  return <div></div>;
+  const [email, setEmal] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <FormContainer>
+      <Form>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter Email'
+            value={email}></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter Password'
+            value={password}></Form.Control>
+        </Form.Group>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default LoginScreen;
