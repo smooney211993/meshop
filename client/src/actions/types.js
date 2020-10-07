b3488a92bbf994f6750871195c4577e5ca1a9781
smooney211993 <sm.athome21@gmail.com>
2020-10-07 13:51:43 +1100

    created user registration types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -20,6 +20,11 @@ export const USER_LOADED_FAIL = 'USER_LOADED_FAIL';
 export const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL';
 export const USER_LOG_OUT = 'USER_LOG_OUT';
 
+// User Registration Types
+export const USER_REGISTER_REQUEST = 'USER_REGISTER_REQUEST';
+export const USER_REGISTER_SUCCESS = 'USER_REGISTER_SUCCESS';
+export const USER_REGISTER_FAIL = 'USER_REGISTER_FAIL';
+
 // alert types
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
