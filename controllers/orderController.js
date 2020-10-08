62202cbae4d00d582aa120f9e09f548b712d3625
smooney211993 <sm.athome21@gmail.com>
2020-10-08 17:06:58 +1100

    created add order items

---
+++ controllers/orderController.js
@@ -0,0 +1,41 @@
+const Order = require('../models/order');
+
+// create new order
+// post/api/order
+
+const addOrderItems = async (req, res, next) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  try {
+    if (orderItems && orderItems.length === 0) {
+      return res.status(400).json({ errors: [{ msg: 'No Order Items' }] });
+    }
+    const order = new Order({
+      orderItems,
+      user: req.user.id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json('Server Error');
+  }
+};
+
+module.exports = {
+  addOrderItems,
+};
