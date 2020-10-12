a34f03d0acb0c9cf01ffeb0fbc2e46d8d564b829
smooney211993 <sm.athome21@gmail.com>
2020-10-13 10:24:29 +1100

    created the userDetails reducer

---
+++ client/src/reducers/userDetails.js
@@ -0,0 +1,26 @@
+import {
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+} from '../actions/types';
+
+const initialState = {
+  user: {},
+  loading: false,
+  error: false,
+};
+
+export default function (state, action) {
+  const { payload, type } = action;
+  switch (key) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCESS:
+      return { ...state, loading: false, user: payload, error: null };
+    case USER_DETAILS_FAIL:
+      return { ...state, loading: false, error: payload };
+
+    default:
+      return state;
+  }
+}
