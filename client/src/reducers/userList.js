eff183d8a03814ce820c2200700011ad6d10cf9d
smooney211993 <sm.athome21@gmail.com>
2020-10-12 16:09:54 +1100

    added in the conditionals in the switch

--- client/src/reducers/userList.js
+++ client/src/reducers/userList.js
@@ -13,6 +13,12 @@ const initialState = {
 export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
+    case USER_LIST_REQUEST:
+      return { ...state, loading: true };
+    case USER_LIST_SUCCESS:
+      return { ...state, loading: false, users: payload, errors: null };
+    case USER_LIST_FAIL:
+      return { ...state, loading: false, error: payload };
     default:
       return state;
   }
