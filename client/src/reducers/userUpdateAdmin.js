caf14429c4e12fcbfc2a2d73263545f0bcaea627
smooney211993 <sm.athome21@gmail.com>
2020-10-14 10:02:56 +1100

    created reducer function for userupdateadmin and added switch statement

--- client/src/reducers/userUpdateAdmin.js
+++ client/src/reducers/userUpdateAdmin.js
@@ -2,4 +2,26 @@ import {
   USER_UPDATE_REQUEST_ADMIN,
   USER_UPATE_SUCCESS_ADMIN,
   USER_UPDATE_FAIL_ADMIN,
+  USER_UPDATE_RESET_ADMIN,
 } from '../actions/types';
+
+const initialState = {
+  loading: null,
+  success: null,
+  error: null,
+};
+
+export default function (state = initialState, action) {
+  const { payload, type } = action;
+  switch (type) {
+    case USER_UPDATE_REQUEST_ADMIN:
+      return { ...state, loading: true };
+    case USER_UPATE_SUCCESS_ADMIN:
+      return { ...state, loading: false, success: true, error: null };
+    case USER_UPDATE_FAIL_ADMIN:
+      return { ...state, loading: false, error: payload };
+
+    default:
+      return state;
+  }
+}
