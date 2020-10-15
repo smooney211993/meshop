b18f10724ff267f06ced21f2a8682adc1720c94e
smooney211993 <sm.athome21@gmail.com>
2020-10-16 10:38:53 +1100

    added in searchbar in history component via route render

--- client/src/Components/Layouts/Header.js
+++ client/src/Components/Layouts/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from '../../actions/userActions';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -22,6 +23,7 @@ const Header = () => {
 
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
+            <Route render={({ history }) => <SearchBar history={history} />} />
             <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
