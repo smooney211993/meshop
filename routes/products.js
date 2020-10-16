21ef28b4a39bebc51db3bbe82e6a793d9cc40fe8
smooney211993 <sm.athome21@gmail.com>
2020-10-16 11:58:35 +1100

    imported gettop products in product route

--- routes/products.js
+++ routes/products.js
@@ -7,12 +7,14 @@ const {
   getProducts,
   getProductsById,
   createNewReview,
+  getTopRatedProducts,
 } = require('../controllers/productController');
 
 // fetch all products
 // public route
 // api/product
 router.route('/').get(getProducts);
+router.get('/top').get(getTopRatedProducts);
 // fetch products by id
 // public route
 // api/product/:id
