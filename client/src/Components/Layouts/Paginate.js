19aafc92d5d22dbefb93b000d3afcb28b877c7f6
smooney211993 <sm.athome21@gmail.com>
2020-10-16 11:27:35 +1100

    added comments explaining logic in paginate component

--- client/src/Components/Layouts/Paginate.js
+++ client/src/Components/Layouts/Paginate.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
+  // make sure pages is greater than 1
+  // take the number of pages and map through it as an array - uses the array constructor and pass in the pages
+  // The link in the linkcontainer depends on if their is a keyword or not
+  // app js has 2 routes for homescreen one with search and if not just the pagenumber
+  // active prop in pagination.Item checks if the current page(x+1) is equal to the current page
   return (
     pages > 1 && (
       <Pagination>
